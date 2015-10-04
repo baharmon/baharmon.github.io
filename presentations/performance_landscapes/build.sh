1780d41a23e7224916c4e515eb7777e96b87f392
baharmon <baharmon@ncsu.edu>
2015-10-04 17:15:16 -0400

    moved presentations to root

--- presentations/performance_landscapes/build.sh
+++
@@ -1,5 +0,0 @@
-./build-slides.py --outdir=build \
-    --title="Flow analysis using sUAS and lidar data" \
-    --meta-description="North Carolina State University, Department of Marine, Earth and Atmospheric Sciences, Graduate Student Seminar" \
-    --meta-author="Helena Mitasova, Vaclav Petras" \
-    foss4ge2015_uav.html
